5eda502521fcd25f78c83c01c7c7c584a9496238
Jithu Jijo <jithujijo@macbooks-MacBook-Pro.local>
2023-06-24 20:05:34 +0530

    edited

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,3 @@
 def call(Map stageParams){
-    checkout scmGit($class: 'GitSCM', branches: [[name: stageParams.branch]], extensions: [], userRemoteConfigs: [[url: stageParams.url]])
+    checkout scmGit(branches: [[name: stageParams.branch]], extensions: [], userRemoteConfigs: [[url: stageParams.url]])
 }
